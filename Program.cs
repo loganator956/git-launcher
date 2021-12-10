46f22d4edbc6bb963be75410d0eeeacc1248ca74
loganator956 <65763483+loganator956@users.noreply.github.com>
2021-12-10 11:24:05 +0000

    created program

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Diagnostics;
+using System;
+
+namespace git_launcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo(Path.Combine(Directory.GetCurrentDirectory(), "git-bash.exe"));
+            startInfo.EnvironmentVariables["HOME"] = Path.Combine(Directory.GetCurrentDirectory(), "home");
+            Console.WriteLine(startInfo.EnvironmentVariables["HOME"]);
+            startInfo.Arguments = "--cd-to-home";
+            Process.Start(startInfo);
+        }
+    }
+}
