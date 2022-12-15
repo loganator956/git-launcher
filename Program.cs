60866055f726124adc08c4115abc6e269cbf26e3
Logan King <65763483+loganator956@users.noreply.github.com>
2022-12-15 18:34:35 +0000

    update to net 6

--- Program.cs
+++ Program.cs
@@ -1,18 +1,7 @@
-﻿using System.IO;
-using System.Diagnostics;
-using System;
+﻿using System.Diagnostics;
 
-namespace git_launcher
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            ProcessStartInfo startInfo = new ProcessStartInfo(Path.Combine(Directory.GetCurrentDirectory(), "git-bash.exe"));
-            startInfo.EnvironmentVariables["HOME"] = Path.Combine(Directory.GetCurrentDirectory(), "home");
-            Console.WriteLine(startInfo.EnvironmentVariables["HOME"]);
-            startInfo.Arguments = "--cd-to-home";
-            Process.Start(startInfo);
-        }
-    }
-}
+ProcessStartInfo startInfo = new ProcessStartInfo(Path.Combine(Directory.GetCurrentDirectory(), "git-bash.exe"));
+startInfo.EnvironmentVariables["HOME"] = Path.Combine(Directory.GetCurrentDirectory(), "home");
+Console.WriteLine(startInfo.EnvironmentVariables["HOME"]);
+startInfo.Arguments = "--cd-to-home";
+Process.Start(startInfo);
