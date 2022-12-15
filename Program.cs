67917f99c1f034201c8b84ad155cd1d550d6b58d
Logan King <65763483+loganator956@users.noreply.github.com>
2022-12-15 19:07:02 +0000

    Add checks for required directories and files. #4

--- Program.cs
+++ Program.cs
@@ -1,8 +1,22 @@
 ﻿using System.Diagnostics;
 
-ProcessStartInfo startInfo = new ProcessStartInfo(Path.Combine(Directory.GetCurrentDirectory(), "git-bash.exe"));
-startInfo.EnvironmentVariables["HOME"] = Path.Combine(Directory.GetCurrentDirectory(), "home");
-Console.WriteLine(startInfo.EnvironmentVariables["HOME"]);
+string gitBashPath = Path.Combine(Directory.GetCurrentDirectory(), "git-bash.exe");
+string homeDirectory = Path.Combine(Directory.GetCurrentDirectory(), "home");
+try
+{
+    if (!File.Exists(gitBashPath))
+        throw new FileNotFoundException($"Can't find git-bash.exe at {gitBashPath}");
+    else if (!Directory.Exists(homeDirectory))
+        throw new DirectoryNotFoundException($"Can't find home directory at {homeDirectory}");
+}
+catch (IOException except)
+{
+    Console.WriteLine(except.ToString());
+    Environment.Exit(1);
+}
+
+ProcessStartInfo startInfo = new ProcessStartInfo(gitBashPath);
+startInfo.EnvironmentVariables["HOME"] = homeDirectory;
 startInfo.ArgumentList.Add("--cd-to-home");
 foreach (string arg in args)
     startInfo.ArgumentList.Add(arg);
